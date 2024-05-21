f3c278c3a168d089052157f1859b493cc134bf72
Eduardo Loback <eduardolloback@gmail.com>
2024-05-21 05:43:43 -0300

    chore: fontconfig

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -53,7 +53,7 @@
   };
 
   # Add stuff for your user as you see fit:
-  programs.neovim.enable = true;
+  fonts.fontconfig.enable = true;
   # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
